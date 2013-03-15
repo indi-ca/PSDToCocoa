4a4f124d7b474192dbe7da08469ca579bc2e5682
Indika Piyasena <indika.piyasena@gmail.com>
2013-03-15 23:21:59 +1100

    UIView creation statements are generated for img_ tags

--- psd_to_cocoa.py
+++ psd_to_cocoa.py
@@ -6,6 +6,9 @@ builds on top of the Slicy App's naming convention.
 
 The PSD is assumed to be in 2x resolution.
 
+img_    tag for an UIImageView
+    assumes that there is a @2x in it
+
 Usage:
    psd_to_cocoa.py <psd>
 
@@ -16,6 +19,8 @@ Options:
 """
 
 # https://github.com/kmike/psd-tools
+import os
+import re
 
 __author__ = "Indika Piyasena"
 
@@ -59,14 +64,18 @@ class PSDToCocoa:
                 self.encode(item, source_bbox)
 
     def encode(self, item, source_bbox):
+        match = re.search(r".*img_.*", item.name)
+        if match:
+            self.render_image(item, source_bbox)
+
         if item.name[:4] == 'view':
             self.render_view(item, source_bbox)
 
         if item.name[:4] == 'image':
             self.render_image(item, source_bbox)
 
-        if item.name[-7:] == '@2x.png':
-            self.render_image(item, source_bbox)
+        # if item.name[-7:] == '@2x.png':
+
 
         source_bbox = item.bbox
         if isinstance(item, Group):
@@ -85,9 +94,23 @@ class PSDToCocoa:
         self.translate(item.bbox, source_bbox)
 
     def render_image(self, item, source_bbox):
-        print "\n\n"
+        print "\n\nImage"
         print item.name
-        self.translate(item.bbox, source_bbox)
+        dim = self.translate(item.bbox, source_bbox)
+
+        basename = os.path.basename(item.name)
+        just_filename = os.path.splitext(basename)[0][:-3]
+
+        CGRect = "({0}, {1}, {2}, {3})".format(dim[0], dim[1], dim[2], dim[3])
+        print CGRect
+        print "{0} = [[UIImageView alloc] initWithFrame:CGRectMake{1}];".format(
+            just_filename, CGRect)
+        print '[{0} setImage:[UIImage imageNamed:@"hypbp_img_title.png"]];'.format(
+            just_filename, just_filename + '.png')
+
+        # hypbp_img_title = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 618, 75)];
+        # [hypbp_img_title setImage:[UIImage imageNamed:@"hypbp_img_title.png"]];
+
 
     def translate(self, bbox, source_bbox):
         d = 2
@@ -107,13 +130,16 @@ class PSDToCocoa:
                                                                 width_1x,
                                                                 height_1x)
 
+        relative = (
+            origin_x - source_origin_x, origin_y - source_origin_y, width_1x,
+            height_1x)
         print "Relative: CGRectMake({0}, {1}, {2}, {3})".format(
             origin_x - source_origin_x, origin_y - source_origin_y,
             width_1x, height_1x)
 
         print "Width: %s" % width_1x
         print "Height: %s" % height_1x
-        pass
+        return relative
 
 
 if __name__ == "__main__":
