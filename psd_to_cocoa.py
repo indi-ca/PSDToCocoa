bc344f217be9dd35216e6609c3b171b41f33b92e
Indika Piyasena <indika.piyasena@gmail.com>
2013-03-20 05:48:48 +1100

    refactoring iOS specific formating to a renderer

--- psd_to_cocoa.py
+++ psd_to_cocoa.py
@@ -20,8 +20,8 @@ Options:
 """
 
 # https://github.com/kmike/psd-tools
-import os
 import re
+from renderers.ios import iOSRenderer
 
 __author__ = "Indika Piyasena"
 
@@ -39,6 +39,7 @@ class PSDToCocoa:
     def __init__(self):
         self.configure_logging()
         self.arguments = docopt(__doc__, version='PSDToCocoa 0.1')
+        self.renderer = iOSRenderer()
         # print(self.arguments)
         pass
 
@@ -67,24 +68,19 @@ class PSDToCocoa:
     def encode(self, item, source_bbox):
         match = re.search(r".*img_.*", item.name)
         if match:
-            self.render_image(item, source_bbox)
+            self.renderer.render_image(item, source_bbox)
 
         match = re.search(r".*btn_.*", item.name)
         if match:
-            self.render_button(item, source_bbox)
+            self.renderer.render_button(item, source_bbox)
 
         match = re.search(r".*view_.*", item.name)
         if match:
-            self.render_view(item, source_bbox)
+            self.renderer.render_view(item, source_bbox)
 
-        # if item.name[:4] == 'view':
-        #     self.render_view(item, source_bbox)
-        #
-        # if item.name[:4] == 'image':
-        #     self.render_image(item, source_bbox)
-
-        # if item.name[-7:] == '@2x.png':
-
+        match = re.search(r".*label_.*", item.name)
+        if match:
+            self.renderer.render_label(item, source_bbox)
 
         source_bbox = item.bbox
         if isinstance(item, Group):
@@ -97,65 +93,19 @@ class PSDToCocoa:
         print item.name
         self.translate(item, source_bbox)
 
-    def render_view(self, item, source_bbox):
-        print "\n\n"
-        print item.name
-        dim = self.translate(item, source_bbox)
-        print "H:|-{0}-[{1}({2})]".format(dim[0], item.name, dim[2])
-        print "V:|-{0}-[{1}({2})]".format(dim[1], item.name, dim[3])
-
-    def render_image(self, item, source_bbox):
-        print "\n\n* Image"
-        print item.name
-        dim = self.translate(item, source_bbox)
-
-        basename = os.path.basename(item.name)
-        just_filename = os.path.splitext(basename)[0][:-3]
-
-        CGRect = "({0}, {1}, {2}, {3})".format(dim[0], dim[1], dim[2], dim[3])
-        print CGRect
-        print "{0} = [[UIImageView alloc] initWithFrame:CGRectMake{1}];".format(
-            just_filename, CGRect)
-        print '[{0} setImage:[UIImage imageNamed:@"{1}"]];'.format(
-            just_filename, just_filename + '.png')
-
-    def render_button(self, item, source_bbox):
-        print "\n\n* Button"
-        print item.name
-        dim = self.translate(item.bbox, source_bbox)
-
-        basename = os.path.basename(item.name)
-        just_filename = os.path.splitext(basename)[0][:-3]
-
-        #post_button = [[UIButton alloc] initWithFrame:CGRectMake(474, 121, 111, 29)];
-        #[post_button setImage:[UIImage imageNamed:@"hypb_btn_comment_default.png"] forState:UIControlStateNormal];
-        #[post_button setImage:[UIImage imageNamed:@"hypb_btn_comment_glow.png"] forState:UIControlStateHighlighted];
-
-        CGRect = "({0}, {1}, {2}, {3})".format(dim[0], dim[1], dim[2], dim[3])
-        print CGRect
-        print "{0} = [[UIButton alloc] initWithFrame:CGRectMake{1}];".format(
-            just_filename, CGRect)
-        print '[{0} setImage:[UIImage imageNamed:@"{1}"] forState:UIControlStateNormal];'.format(
-            just_filename, just_filename + '.png')
-        print '[{0} setImage:[UIImage imageNamed:@"{1}"] forState:UIControlStateHighlighted];'.format(
-            just_filename, just_filename + '.png')
-
-
     def translate(self, item, source_bbox):
         d = 2
 
         bbox = item.bbox
         # Seems as though the bounding box is 1px larger than the layer
-        origin_x = (bbox.x1) / d
-        origin_y = (bbox.y1) / d
+        origin_x = bbox.x1 / d
+        origin_y = bbox.y1 / d
 
-        source_origin_x = (source_bbox.x1) / d
-        source_origin_y = (source_bbox.y1) / d
+        source_origin_x = source_bbox.x1 / d
+        source_origin_y = source_bbox.y1 / d
 
-        # width_1x = (bbox.width - 2) / d
-        width_1x = (bbox.width) / d
-        # height_1x = (bbox.height - 2) / d
-        height_1x = (bbox.height) / d
+        width_1x = bbox.width / d
+        height_1x = bbox.height / d
 
         print "Absolute: CGRectMake({0}, {1}, {2}, {3})".format(origin_x,
                                                                 origin_y,
--- renderers/ios.py
+++ renderers/ios.py
@@ -1,3 +1,5 @@
+import os
+
 __author__ = "Indika Piyasena"
 
 
@@ -5,17 +7,87 @@ class iOSRenderer:
     def __init__(self):
         pass
 
-    def process(self):
-        pass
+    def render_view(self, item, source_bbox):
+        print "\n\n"
+        print item.name
+        dim = self.translate(item, source_bbox)
+        print "H:|-{0}-[{1}({2})]".format(dim[0], item.name, dim[2])
+        print "V:|-{0}-[{1}({2})]".format(dim[1], item.name, dim[3])
 
-    def log(self):
-        pass
+    def render_label(self, item, source_bbox):
+        print "\n\n"
+        print item.name
+        dim = self.translate(item, source_bbox)
+        print "H:|-{0}-[{1}({2})]".format(dim[0], item.name, dim[2])
+        print "V:|-{0}-[{1}({2})]".format(dim[1], item.name, dim[3])
+
+    def render_image(self, item, source_bbox):
+        print "\n\n* Image"
+        print item.name
+        dim = self.translate(item, source_bbox)
+
+        basename = os.path.basename(item.name)
+        just_filename = os.path.splitext(basename)[0][:-3]
+
+        CGRect = "({0}, {1}, {2}, {3})".format(dim[0], dim[1], dim[2], dim[3])
+        print CGRect
+        print "{0} = [[UIImageView alloc] initWithFrame:CGRectMake{1}];".format(
+            just_filename, CGRect)
+        print '[{0} setImage:[UIImage imageNamed:@"{1}"]];'.format(
+            just_filename, just_filename + '.png')
+        print '@"H:|-{0}-[{1}({2})]",'.format(dim[0], just_filename, dim[2])
+        print '@"V:|-{0}-[{1}({2})]",'.format(dim[1], just_filename, dim[3])
+
+
+    def render_button(self, item, source_bbox):
+        print "\n\n* Button"
+        print item.name
+        dim = self.translate(item.bbox, source_bbox)
+
+        basename = os.path.basename(item.name)
+        just_filename = os.path.splitext(basename)[0][:-3]
+
+        CGRect = "({0}, {1}, {2}, {3})".format(dim[0], dim[1], dim[2], dim[3])
+        print CGRect
+        print "{0} = [[UIButton alloc] initWithFrame:CGRectMake{1}];".format(
+            just_filename, CGRect)
+        print '[{0} setImage:[UIImage imageNamed:@"{1}"] forState:UIControlStateNormal];'.format(
+            just_filename, just_filename + '.png')
+        print '[{0} setImage:[UIImage imageNamed:@"{1}"] forState:UIControlStateHighlighted];'.format(
+            just_filename, just_filename + '.png')
+
+    def translate(self, item, source_bbox):
+        d = 2
+
+        bbox = item.bbox
+        # Seems as though the bounding box is 1px larger than the layer
+        origin_x = bbox.x1 / d
+        origin_y = bbox.y1 / d
+
+        source_origin_x = source_bbox.x1 / d
+        source_origin_y = source_bbox.y1 / d
+
+        width_1x = bbox.width / d
+        height_1x = bbox.height / d
+
+        print "Absolute: CGRectMake({0}, {1}, {2}, {3})".format(origin_x,
+                                                                origin_y,
+                                                                width_1x,
+                                                                height_1x)
+
+        relative = (
+            origin_x - source_origin_x, origin_y - source_origin_y, width_1x,
+            height_1x)
+        print "Relative: CGRectMake({0}, {1}, {2}, {3})".format(
+            origin_x - source_origin_x, origin_y - source_origin_y,
+            width_1x, height_1x)
+
+        print "Width: %s" % width_1x
+        print "Height: %s" % height_1x
+        return relative
 
 
 if __name__ == "__main__":
     print "Running iOSRenderer in stand-alone-mode"
 
     ios_renderer = iOSRenderer()
-    ios_renderer.process()
-
-    ios_renderer.log()
